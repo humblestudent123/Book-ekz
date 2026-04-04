a73a787157c590213b86d35c5becd64cf3fb2e32
Bruh <vasickinfedor@gmail.com>
2026-04-04 12:46:17 +0300

    main-page-changes

--- src/api/booksApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/books';
-
-export const getBooks = () => axios.get(API_URL).then(res => res.data);
-export const addBook = (book) => axios.post(API_URL, book).then(res => res.data);
-export const deleteBook = (id) => axios.delete(`${API_URL}/${id}`).then(res => res.data);
