6b3e4f334039141d92cf7e5e55bba6560c1c726b
Bruh <vasickinfedor@gmail.com>
2026-04-20 17:30:02 +0300

    style-cover-change

--- src/entities/book/BookCard.jsx
+++ src/entities/book/BookCard.jsx
@@ -4,16 +4,21 @@ import PropTypes from 'prop-types';
 export default function BookCard({ book, onSelect }) {
   return (
     <div className="book-card" onClick={() => onSelect(book)}>
-      <h3>{book.title}</h3>
-      <p>{book.author} · {book.year}</p>
-      <div style={{ marginTop: '10px' }}>
-        {book.tags.map(t => <span key={t} className="tag">{t}</span>)}
+      
+      <div className="book-card__image-wrapper">
+        <img
+          src={book.cover}
+          alt={book.title}
+          className="book-card__cover"
+          loading="lazy"
+        />
       </div>
-    </div>
-  );
-}
 
-BookCard.propTypes = {
-  book: PropTypes.object.isRequired,
-  onSelect: PropTypes.func.isRequired,
-};
+      <div className="book-card__info">
+        <h3>{book.title}</h3>
+        <p>{book.author}</p>
+      </div>
+
+    </div>
+  );  
+}
